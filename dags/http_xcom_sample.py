b59357b58e5c0c531dc1cd3485fd4b1f5938f4d9
jujumilk3 <jujumilk3@gmail.com>
2022-05-05 20:24:37 +0900

    Add http_xcom_sample

---
+++ dags/http_xcom_sample.py
@@ -0,0 +1,97 @@
+import json
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
+
+
+def handle_response(response, **context):
+    print(response)
+    print(response.__dict__)
+    print(response.content)
+    response_json_as_dict = json.loads(response.content)
+    print(response_json_as_dict)
+    if str(response.status_code).startswith('2'):  # to catch 2XX http status code
+        context['task_instance'].xcom_push(key='base_task_xcom', value='success')  # 이건 안됨이 아니라 잘됨.
+        context['task_instance'].xcom_push(key='second_task_number', value=response_json_as_dict.get('next_task_number', 1))
+        return True
+    else:
+        context['task_instance'].xcom_push(key='base_task_xcom', value='fail')  # 애초에 다음으로 진행이 안되니 무의미
+        return False
+
+
+def treat_as_branch(**context):
+    print("Here is treat_as_branch")
+    print(context)
+    base_task_result = context['task_instance'].xcom_pull(key='base_task_xcom')
+    next_task_number = context['task_instance'].xcom_pull(key='second_task_number')
+    print("This is base_task_result")
+    print(base_task_result)
+    return 'http_dummy_task' + str(next_task_number)
+
+
+def get_http_payload(**context):
+    http_payload = context['ti'].xcom_pull(task_ids='base_task')
+    print("this is base_task's http payload")
+    print(http_payload)  # got returns as string
+    http_payload2 = context['ti'].xcom_pull(task_ids='base_task2')
+    print("this is base_task's http payload")
+    print(http_payload2)  # got returns as string
+    test_xcom_value = context['ti'].xcom_pull(key='xcom_push_value')
+    print("this is xcom value")
+    print(test_xcom_value)  # 안되는거 세트 가 아니라 아주 잘 됨.
+
+
+def complete(**context):
+    print(context)
+
+
+with DAG(
+    'http_xcom_sample',
+    description='A simple http DAG',
+    schedule_interval=timedelta(hours=1),
+    start_date=datetime(2021, 1, 1),
+    catchup=False,
+    tags=['example'],
+) as dag:
+    base_task = SimpleHttpOperator(
+        task_id='base_task',
+        method='GET',
+        endpoint='/airflow/base-task',
+        http_conn_id='localhost',
+        response_check=handle_response,
+    )
+
+    branch_task = BranchPythonOperator(
+        task_id='branch_task',
+        python_callable=treat_as_branch
+    )
+
+    http_dummy_task1 = SimpleHttpOperator(
+        task_id='http_dummy_task1',
+        method='GET',
+        endpoint='/airflow/dummy-task1',
+        http_conn_id='localhost',
+    )
+
+    http_dummy_task2 = SimpleHttpOperator(
+        task_id='http_dummy_task2',
+        method='GET',
+        endpoint='/airflow/dummy-task2',
+        http_conn_id='localhost',
+    )
+
+    http_dummy_task3 = SimpleHttpOperator(
+        task_id='http_dummy_task3',
+        method='GET',
+        endpoint='/airflow/dummy-task3',
+        http_conn_id='localhost',
+    )
+
+    complete_task = PythonOperator(
+        task_id='complete_task',
+        python_callable=complete
+    )
+
+    base_task >> branch_task >> [http_dummy_task1, http_dummy_task2, http_dummy_task3] >> complete_task
